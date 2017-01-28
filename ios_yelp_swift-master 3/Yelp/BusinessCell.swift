8c8bdbc2915c01d617ec567bca794c800c8d0298
Brandon Sanchez <brandonsanchez05@gmail.com>
2017-01-28 01:22:25 -0500

    Finished autolayout on tableview cells

---
+++ ios_yelp_swift-master 3/Yelp/BusinessCell.swift
@@ -0,0 +1,47 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Brandon Sanchez on 1/27/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet var thumbImageView: UIImageView!
+    @IBOutlet var ratingImageView: UIImageView!
+    @IBOutlet var reviewsCountLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var categoriesLabel: UILabel!
+    @IBOutlet var distanceLabel: UILabel!
+    
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            thumbImageView.setImageWith(business.imageURL!)
+            ratingImageView.setImageWith(business.ratingImageURL!)
+            reviewsCountLabel.text = String(describing: business.reviewCount!) + " Reviews"
+            categoriesLabel.text = business.categories
+            distanceLabel.text = business.distance
+            addressLabel.text = business.address
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        thumbImageView.layer.cornerRadius = 4
+        thumbImageView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
